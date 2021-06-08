c61204aaf1065dc8d13cffcdfa4f993c40add951
HazimHasnan <hazim.hasnan@xpress.ai>
2021-06-09 01:09:55 +0800

    Refactored the code

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -12,14 +12,14 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
